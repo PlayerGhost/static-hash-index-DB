949cceb3a3bb67ffd831bcdb7b7d0f793b3b9f7f
PauloUbirajara <ubirajara3@hotmail.com>
2022-02-22 10:34:51 -0300

    Separar obtenção e formatação de palavras do repositório

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-import requests
+from requests import get
+from requests.status_codes import codes as HTTPStatusCode
+
+ENGLISH_WORDS_URL = 'https://github.com/dwyl/english-words/raw/master/words_dictionary.json'
+
+
+def load_database():
+    wordlist_json = get_words_from_repo()
+    result = wordlist_json.keys()
+
+    return result
+
+
+def get_words_from_repo():
+    response = get(ENGLISH_WORDS_URL)
 
     if response.status_code != HTTPStatusCode.OK:
-        invalid_response_error = 'Houve algum erro durante a obtenção dos dados'
+        invalid_response_error = 'Não foi possível obter a lista de palavras do repositório!'
         raise ValueError(invalid_response_error)
 
-    return list(dataDict.items())
+    return response.json()
+
 
 if __name__ == '__main__':
-    database = loadDatabase()
+    database = load_database()
 
     print(database)
