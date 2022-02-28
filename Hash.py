3b11c13c0030666541881ee43fa0c00dbb28c7cf
Player Ghost <sousaitalo12@gmail.com>
2022-02-28 19:35:43 -0300

    buckets overflow quase la

--- Hash.py
+++ Hash.py
@@ -1,19 +1,18 @@
 import math
 
-
 def hash(registro, bucketSize):
 
     index = math.trunc(len(registro) / 2)
     half = ord(registro[index])
-    print(registro,registro[index])
+    #print(registro, registro[index])
 
     for i in range(index + 1, len(registro)):
         if i % 2 == 0:
-            half = half + ord(registro[i])
+            half = (half + ord(registro[i])*2)*i
         else:
-            half = half - ord(registro[i])
+            half = (half - int(ord(registro[i])/3))*i
 
     for j in range(0, index):
         half += ord(registro[j])
 
-    return abs(half % bucketSize)
+    return int(abs(half % bucketSize))
--- main.py
+++ main.py
@@ -19,16 +19,70 @@ def get_words_from_repo():
     return response
 
 
+def makePages(tabela, pageSize):
+    return [tabela[i:i + pageSize] for i in range(0, len(tabela), pageSize)]
+
+
 if __name__ == '__main__':
-    database = load_database()
-    database.pop(-1)
-    teste = set()
-    print(database[-1])
+    tabela = load_database()
+    tabela.pop(-1)
 
-    for i in database:
+    pageSize = 10
+    paginas = makePages(tabela, pageSize)
 
-       teste.add(hash(i,len(database)))
+    nb = len(tabela) // 5
+    buckets = [[None for i in range(0, 5)] for i in range(0, nb)]
+    #print(nb)
+    #print(buckets)
+    #93310
 
-    print('tamanho do set ', len(teste))
-    print('tamanho do database ', len(database))
+    for i in range(0, len(paginas)):
+        for j in range(0, len(paginas[i])):
+            hashIndex = hash(paginas[i][j], nb)
 
+            if buckets[hashIndex][-1] is not None:
+                hasSlot = False
+
+                for k in range(0, len(buckets[hashIndex])):
+                    if type(buckets[hashIndex][k]) == list:
+                        for l in range(0, len(buckets[hashIndex][k])):
+                            if buckets[hashIndex][k][l] is None:
+                                buckets[hashIndex][k][l] = (paginas[i][j], i)
+                                hasSlot = True
+                                break
+
+                        break
+                if not hasSlot:
+                    bucketOverflow = [None for i in range(0, 5)]
+                    bucketOverflow[0] = (paginas[i][j], i)
+
+                    buckets[hashIndex].append(bucketOverflow)
+            else:
+                for k in range(0, len(buckets[hashIndex])):
+                    if buckets[hashIndex][k] is None:
+                        buckets[hashIndex][k] = (paginas[i][j], i)
+                        break
+
+    aux = [0 for i in range(0, len(buckets))]
+
+    for i in range(0, len(tabela)):
+        aux[hash(tabela[i], nb)] += 1
+
+    print(max(aux), aux.index(max(aux)))
+
+    print(buckets[aux.index(max(aux))])
+
+    print(buckets)
+
+    #teste = set()
+    #print(tabela[-1])
+
+    #for i in tabela:
+    #    teste.add(hash(i, nb))
+
+    #print(max(teste))
+
+    #print(len(tabela) / len(teste))
+
+    #print('tamanho do set ', len(teste))
+    #print('tamanho do database ', len(tabela))
