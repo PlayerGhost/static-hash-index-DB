d757ea551d0e9cc7268be7ef6c878072a1a9aad5
PauloUbirajara <ubirajara3@hotmail.com>
2022-03-09 01:02:24 -0300

    Enviar response para frontend por flask

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+from json import dumps
 
 from main import Programa
 
@@ -6,12 +7,21 @@ app = Flask(__name__)
 programa = Programa()
 
 
+def new_response(content=''):
+    res = Response()
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.set_data(content.encode('utf8'))
+    return res
+
+
 @app.route("/")
 def main():
     tamanhoPagina = int(request.args.get('pageSize'))
     programa.setup(tamanhoPagina)
-    print(programa.numeroBuckets)
-    return 'teste'
+
+    response = new_response()
+
+    return response
 
 
 if __name__ == '__main__':
